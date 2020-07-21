af0e3608b51f25c47fc20798d9411e2f7877d8ca
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-07-21 19:51:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -54,7 +54,7 @@ while True:
           sock.sendto(bytes, (ip,port))
           sent = sent + 1
           port = port + 1
-          print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+          print ("Sent "+sent+" packet to "+ip+" throught port:"+port)
           if port == 65534:
              port = 1
      except:
